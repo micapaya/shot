0e587aeb907c7154ab1bd51dbb1424bb98973437
micapaya <mica.payage@gmail.com>
2019-01-10 16:59:24 +0100

    add design for score and +1 icon naimate

--- js/scoreRules.js
+++ js/scoreRules.js
@@ -1,8 +1,20 @@
 let shoot = 0;
 
-function scoring() 
-{ 
+function scoring() { 
 shoot = shoot+1; 
 document.getElementById('nb').innerHTML = shoot; 
-
 } 
+
+let score = bodymovin.loadAnimation({
+	container: document.getElementById('item'),
+	renderer: 'svg',
+	autoplay: false,
+	loop: false,
+	path: '../scorePlus1.json',
+});
+
+function playingscore () {
+	score.playSegments([58,80], true);
+
+}
+
