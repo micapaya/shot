ecd325150f4f304ccd10e1cdacf224fb4aa80a51
micapaya <mica.payage@gmail.com>
2019-01-11 14:15:21 +0100

    add sound

--- js/target.js
+++ js/target.js
@@ -17,14 +17,24 @@ function removeHide (){
 	target.classList.add("show");
 	BallTarget.playSegments([1,20],true);
 }
+function spaceSound() {
+	let sound2 = document.getElementById("playerSpace");
+	sound2.play();
+}
+function sheepSound() {
+    let sound = document.getElementById("playerSheep");
+    sound.play();            
+}
 
 target.addEventListener("click", function( event ) {   
     // met en surbrillance la cible de mouseenter
-     BallTarget.playSegments([80,100],true);
+     BallTarget.playSegments([60,100],true);
      setTimeout(addHide, 3000);
      setTimeout(removeHide, 5000);
+     setTimeout(sheepSound, 250);
      
 });
+
 
 
 
